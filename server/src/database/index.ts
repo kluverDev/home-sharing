acf3998a6d24d2b707b7810c11496e51d671bafa
wizzy akor <kluverdev@gmail.com>
2020-05-19 16:55:58 -0400

    added env variables and also connect app to mongo atlas database

---
+++ server/src/database/index.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { MongoClient } from "mongodb";
+require("dotenv").config();
+import { Database } from "../lib/types";
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/test?retryWrites=true&w=majority`;
+export const connectDatabase = async (): Promise<Database> => {
+  const client = await MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db("main");
+  return {
+    listings: db.collection("test_listings"),
+  };
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,53 +1,25 @@
-import express = require("express");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import express, { Application } from "express";
 import bodyParser from "body-parser";
+require("dotenv").config();
+import { connectDatabase } from "./database";
 import { ApolloServer } from "apollo-server-express";
 import { typeDefs } from "./graphql/typeDefs";
-import {resolvers} from "./graphql/resolvers"
+import { resolvers } from "./graphql/resolvers";
 
+const mount = async (app: Application) => {
+  const db = await connectDatabase();
+  app.use(bodyParser.json());
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: () => ({ db }), //provides db in all files via context
+  }); // my appolo server
+  server.applyMiddleware({ app, path: "/api" }); //specifying express app instance & graphql api
 
-const app = express(); //my express app
-const port = 9000;
-app.use(bodyParser.json());
-const server = new ApolloServer({ typeDefs, resolvers });// my appolo server
-server.applyMiddleware({ app, path: "/api" });//specifying express app instance & graphql api
+  const listings = await db.listings.find({}).toArray();
+  console.log(listings);
+  console.log(`[app] : http://localhost:${process.env.PORT}`);
+};
 
-
-
-app.listen(port);
-console.log(`[app] : http://localhost:${port}`);
+mount(express());
