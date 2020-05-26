c8082debb3e63ac9ca6d2040f062abca3a4e7b59
wizzy akor <kluverdev@gmail.com>
2020-05-26 13:41:19 -0400

    Database document structure. Added more types/interfaces

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -11,15 +11,60 @@ export interface Listing {
   numOfBaths: number;
   rating: number;
 }
+
 export interface Booking {
   _id: ObjectId;
 }
-export interface User {
-  _id: ObjectId;
+export enum ListingType {
+  Apartment = "APARTMENT",
+  House = "HOUSE",
 }
-
+export interface User {
+  _id: string;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  walletId?: string;
+  income: number;
+  bookings: ObjectId[];
+  listings: ObjectId[];
+}
 export interface Database {
   listings: Collection<Listing>;
   users: Collection<User>;
   bookings: Collection<Booking>;
+}
+interface BookingsIndexMonth {
+  [key: string]: boolean;
+}
+
+interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth;
+}
+
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear;
+}
+export interface Listing {
+  _id: ObjectId;
+  title: string;
+  description: string;
+  image: string;
+  host: string;
+  type: ListingType;
+  address: string;
+  country: string;
+  admin: string;
+  city: string;
+  bookings: ObjectId[];
+  bookingsIndex: BookingsIndex;
+  price: number;
+}
+export interface Booking {
+  _id: ObjectId;
+  listing: ObjectId;
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
 }
