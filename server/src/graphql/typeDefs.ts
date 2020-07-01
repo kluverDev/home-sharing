581ea0cba305961fe3c1f432c1a8cafac9ce884c
wizzy akor <kluverdev@gmail.com>
2020-07-01 15:07:53 -0400

    Modifying the User, Listing, and Booking GraphQL TypeDefs

--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,6 +1,51 @@
 import { gql } from "apollo-server-express";
 
 export const typeDefs = gql`
+  type Bookings {
+    total: Int!
+    result: [Booking!]!
+  }
+  type Booking {
+    id: ID!
+    listing: Listing!
+    tenant: User!
+    checkIn: String!
+    checkOut: String!
+  }
+  type Listing {
+    id: ID!
+    title: String!
+    description: String!
+    image: String!
+    host: User!
+    type: ListingType!
+    address: String!
+    city: String!
+    bookings(limit: Int!, page: Int!): Bookings
+    bookingsIndex: String!
+    price: Int!
+    numOfGuests: Int!
+  }
+  enum ListingType {
+    APARTMENT
+    HOUSE
+  }
+
+  type Listings {
+    total: Int!
+    result: [Listing!]!
+  }
+  type User {
+    id: ID!
+    name: String!
+    avatar: String!
+    contact: String!
+    hasWallet: Boolean!
+    income: Int
+    bookings(limit: Int!, page: Int!): Bookings
+    listings(limit: Int!, page: Int!): Listings!
+  }
+
   type Viewer {
     id: ID
     token: String
@@ -14,7 +59,7 @@ export const typeDefs = gql`
 
   type Query {
     authUrl: String!
-    user: String!
+    user(id: ID!): User!
   }
 
   type Mutation {
